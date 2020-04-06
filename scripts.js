b76722687a7289a495844854604aeb90fe09717c
LAMI <xavier.lami@edu.devinci.fr>
2020-04-06 16:01:58 -0400

    Exercice completed

--- scripts.js
+++ scripts.js
@@ -6,11 +6,12 @@ let firstCard,secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if(this === firstCard) return;
 
     this.classList.add('flip');
 
     if(!hasFlippedCard){
-        // premier clic 
+        // premier clic
         hasFlippedCard=true;
         firstCard=this;
 
@@ -31,7 +32,9 @@ function checkForMatch() {
 
 function disableCards() {
     flipCard.removeEventListener('click',flipCard);
-    secondCard.removeEventListener('click',flipCard); 
+    secondCard.removeEventListener('click',flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
@@ -41,9 +44,22 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockBoard = false;
-        }, 1500);  
+        resetBoard();
+        }, 1500);
 }
+
+function resetBoard(){
+  [hasFlippedCard,lockBoard]=[false,false];
+  [firstCard,secondCard]=[null,null];
+
+}
+
+(function shuffle(){
+  cards.forEach(card =>{
+    let randomPos = Math.floor(Math.random()*12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard))
 
