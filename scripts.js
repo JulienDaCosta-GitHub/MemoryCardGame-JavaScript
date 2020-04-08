8c6e2f2d87d901e5c5f4d233861dc9bca8e51b97
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-04-08 02:35:29 +0200

    Comments

--- scripts.js
+++ scripts.js
@@ -1,42 +1,48 @@
+// Retrouver les cartes ayant la bonne classe
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard= false;
+// Définition de variables
+let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard,secondCard;
+let firstCard, secondCard;
 
+// Ajout de la classe flip a la carte séléctionnée
 function flipCard() {
     if (lockBoard) return;
-    if(this === firstCard) return;
+    if (this === firstCard) return;
 
     this.classList.add('flip');
 
-    if(!hasFlippedCard){
-        // premier clic
-        hasFlippedCard=true;
-        firstCard=this;
+    if (!hasFlippedCard) {
+        //  Premier click
+        hasFlippedCard = true;
+        firstCard = this;
 
         return;
     }
-        // second clic
-        hasFlippedCard=false;
-        secondCard= this;
+        // Second click
+        hasFlippedCard = false;
+        secondCard = this;
 
         checkForMatch();
 }
 
+// Si il y a une correspondance entre les deux cartes elles sont bloquées, sinon on les retourne
 function checkForMatch() {
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
     isMatch ? disableCards() : unflipCards();
 }
 
+// Pour bloquer les cartes = Empécher de re-cliquer dessus
 function disableCards() {
-    flipCard.removeEventListener('click',flipCard);
-    secondCard.removeEventListener('click',flipCard);
+    flipCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
 }
 
+// Si il n'y a pas de match, cette fonction est appelée pour bloquer les cartes pendant 1.5 secondes, puis les retourner
 function unflipCards() {
     lockBoard = true;
 
@@ -48,12 +54,13 @@ function unflipCards() {
         }, 1500);
 }
 
-function resetBoard(){
-  [hasFlippedCard,lockBoard]=[false,false];
-  [firstCard,secondCard]=[null,null];
-
+// Permet de réinitialiser les valeurs par défault des variables (définit en haut du script)
+function resetBoard() {
+  [hasFlippedCard,lockBoard] = [false,false];
+  [firstCard,secondCard] = [null,null];
 }
 
+// Mélanger les cartes
 (function shuffle(){
   cards.forEach(card =>{
     let randomPos = Math.floor(Math.random()*12);
@@ -61,5 +68,6 @@ function resetBoard(){
   });
 })();
 
+// Enregsitrer le click sur les cartes et effectuer la fonction flipCard
 cards.forEach(card => card.addEventListener('click', flipCard))
 
